8832d8f1e36ad3a3b112bdb13e17a777ae3f2d10
Ronan Fruit <rfruit@fb.com>
2019-01-17 18:45:32 -0500

    Fixed bug with tensorboards

--- pytorch/tensorboard_test.py
+++ pytorch/tensorboard_test.py
@@ -1,16 +1,17 @@
 import math as m
 from tensorboardX import SummaryWriter
+import sys
 
+print(sys.executable)
 
-writer = SummaryWriter()
+writer = SummaryWriter('runs')
 
-funcs = {"sin":m.sin, "cos":m.cos, "tan":m.tan}
+# funcs = {"sin":m.sin, "cos":m.cos, "tan":m.tan}
+funcs = {"sin":m.sin}
 
-for angle in range(0,360):
+for angle in range(0, 360):
     angle_rad = angle * m.pi/180
     for name, fun in funcs.items():
         val = fun(angle_rad)
         writer.add_scalar(name, val, angle)
-    writer.close()
-
-# TODO: fix bug when calling add_scalar...
+writer.close()
